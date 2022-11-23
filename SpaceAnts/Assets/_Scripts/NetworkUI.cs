711759bcf75a07d1eb8a8eb30bfc9c1290413209
Yannik <yannik.braendle@t-online.de>
2022-11-23 23:25:27 +0100

    added network fields

--- SpaceAnts/Assets/_Scripts/NetworkUI.cs
+++ SpaceAnts/Assets/_Scripts/NetworkUI.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
-
+using TMPro;
+using Unity.Netcode.Transports.UTP;
 public class NetworkUI : MonoBehaviour
 {
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private TMP_InputField IPInput;
+    [SerializeField] private TMP_InputField PortInput;
 
 
     private void Awake()
@@ -19,10 +22,14 @@ public class NetworkUI : MonoBehaviour
         });
         clientBtn.onClick.AddListener(() =>
         {
+            var utp = (UnityTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+            utp.SetConnectionData(IPInput.text, ushort.Parse(PortInput.text));
             NetworkManager.Singleton.StartClient();
         });
         hostBtn.onClick.AddListener(() =>
         {
+            var utp = (UnityTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+            utp.SetConnectionData(IPInput.text, ushort.Parse(PortInput.text));
             NetworkManager.Singleton.StartHost();
         });
     }
