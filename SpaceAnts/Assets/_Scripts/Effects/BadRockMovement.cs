5fbdabd454187452a3bcaa10df6f081be54116c6
chocoland.tv <r.moegenburg@t-online.de>
2022-11-28 20:16:07 +0100

    BackRockMovement

---
+++ SpaceAnts/Assets/_Scripts/Effects/BadRockMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadRockMovement : MonoBehaviour
+{
+    private GameObject mothership;
+    private Vector3 startPosition; 
+    private float finishTime;
+    private float delayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mothership = GameObject.FindGameObjectWithTag("HomeBase");
+        finishTime = (Random.Range ( 0f, 1f))*50;
+        startPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delayTime += Time.deltaTime/ finishTime;
+        transform.position = Vector3.Lerp(startPosition,mothership.transform.position,delayTime);
+    }
+}
